481a088a40f4eb2abaee5ba108238d5383a446fc
kmilek <kmilek73@gmail.com>
2022-12-05 13:36:53 +0100

    rozwiązanie

---
+++ JS/main.js
@@ -0,0 +1,11 @@
+let firstNumber = 10;
+let secondNumber = 11;
+
+
+function dodawanie (firstNumber, secondNumber){
+let suma = firstNumber + secondNumber;
+return suma;
+}
+
+console.log (dodawanie(firstNumber, secondNumber)); /* wywołanie funkcji w konsoli */
+
